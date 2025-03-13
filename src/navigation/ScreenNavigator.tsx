bdb5a98640f1b2566809f699e4e98a09d42c5112
dhruv <dhruv.inbweb25@gmail.com>
2025-03-13 15:24:35 +0530

    update

--- src/navigation/ScreenNavigator.tsx
+++ src/navigation/ScreenNavigator.tsx
@@ -1,16 +1,20 @@
-import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Login from "../screens/Login"; // Adjust the path as necessary
-import Home from "../screens/Home";   // Adjust the path as necessary
+import React from "react";
+import Home from "../screens/Home";
+import Login from "../screens/Login";
 import Speakers from "../screens/Speakers";
 
 const ScreenStack = createStackNavigator();
 
 const ScreenNavigator = () => {
   return (
-    <ScreenStack.Navigator initialRouteName="Speakers" screenOptions={{ headerShown: false }}>
-      <ScreenStack.Screen name="Home" component={Home}/>
-      <ScreenStack.Screen name="Speakers" component={Speakers}/>
+    <ScreenStack.Navigator
+      initialRouteName="Speakers"
+      screenOptions={{ headerShown: false }}
+    >
+      <ScreenStack.Screen name="Home" component={Home} />
+      <ScreenStack.Screen name="Speakers" component={Speakers} />
+      <ScreenStack.Screen name="Login" component={Login} />
     </ScreenStack.Navigator>
   );
 };
